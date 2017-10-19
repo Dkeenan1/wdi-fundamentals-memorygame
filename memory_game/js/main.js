f79648efe363ccefc2dabe8ac80c3dfbb35880dd
Daniel Keenan <devildanny5@Daniel-Keenans-MacBook-Pro.local>
2017-10-19 16:00:09 -0400

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,33 +3,26 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped" + " " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped" + " " + cardTwo);
-
-var cardThree = cards[2];
-cardsInPlay.push(cardThree);
-
-console.log("User flipped" + " " + cardThree);
-
-var cardFour = cards[3];
-cardsInPlay.push(cardFour);
-
-console.log("User flipped" + " " + cardFour);
-
-if (cardsInPlay.length = 2) {
+var checkForMatch = function(){
 if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 }
-if (cardsInPlay[0] !== cardsInPlay[1]) {
+else {
 	alert("Sorry, try again");
 }
+};
+var flipCard = function(cardId) {
+
+console.log("User Flipped " + cards[cardId]);
+
+if (cardsInPlay.length = 2) {
 }
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+cardsInPlay.push(cards[cardId]);
+
 
